7b54e6eb014d9e9ba000960036a0fad1e4c78675
Angel <2643152157@qq.com>
2025-08-19 21:00:55 +0800

    8.19

---
+++ src/app/documents/[documentId]/editor.tsx
@@ -0,0 +1,101 @@
+"use client";
+
+import { useEditor, EditorContent } from "@tiptap/react";
+import StarterKit from "@tiptap/starter-kit";
+import TaskItem from "@tiptap/extension-task-item";
+import TaskList from "@tiptap/extension-task-list";
+import Image from "@tiptap/extension-image";
+import ImageResize from "tiptap-extension-resize-image";
+import Underline from "@tiptap/extension-underline";
+import TextStyle from "@tiptap/extension-text-style";
+import FontFamily from "@tiptap/extension-font-family";
+import Table from "@tiptap/extension-table";
+import TableCell from "@tiptap/extension-table-cell";
+import TableHeader from "@tiptap/extension-table-header";
+import TableRow from "@tiptap/extension-table-row";
+import { Color } from "@tiptap/extension-color";
+import Highlight from "@tiptap/extension-highlight";
+import Link from "@tiptap/extension-link";
+import TextAlign from "@tiptap/extension-text-align";
+import { FontSizeExtension } from "@/app/extensions/font-size";
+import { useEditorState } from "@/store/use-editor-store";
+const Editor = () => {
+  // useEditor 是 Tiptap 提供的 React Hook，用于 --> 创建编辑器实例
+  const { setEditor } = useEditorState();
+  const editor = useEditor({
+    onCreate({ editor }) {
+      setEditor(editor);
+    },
+    onDestroy() {
+      setEditor(null);
+    },
+    onUpdate({ editor }) {
+      setEditor(editor);
+    },
+    onSelectionUpdate({ editor }) {
+      setEditor(editor);
+    },
+    onTransaction({ editor }) {
+      setEditor(editor);
+    },
+    onFocus({ editor }) {
+      setEditor(editor);
+    },
+    onBlur({ editor }) {
+      setEditor(editor);
+    },
+    onContentError({ editor }) {
+      setEditor(editor);
+    },
+
+    editorProps: {
+      // 直接映射到编辑区域的属性（DOM props）
+      attributes: {
+        style: "padding-left:56px;padding-right:56px", // 内联样式
+        // 类名
+        class:
+          "focus:outline-none bg-white border border-[#C7C7C7] print:border-0 flex flex-col min-h-[1054px] w-[816px] pt-5 pr-14 pb-10 cursor-text",
+      },
+    },
+    extensions: [
+      StarterKit,
+      FontSizeExtension,
+      TextAlign.configure({
+        types: ["heading", "paragraph"],
+      }),
+      Link.configure({
+        openOnClick: false,
+        autolink: true,
+        defaultProtocol: "https",
+      }),
+      Color,
+      Highlight.configure({ multicolor: true }),
+      TextStyle,
+      FontFamily,
+      Underline,
+      TaskList,
+      TaskItem.configure({
+        nested: true,
+      }),
+      Table.configure({
+        resizable: true,
+      }),
+      TableRow,
+      TableHeader,
+      TableCell,
+      Image,
+      ImageResize,
+    ], // 基础编辑器功能集合
+    content: "<p>Hello World! 🌎️</p>",
+  });
+
+  return (
+    <div className="size-full overflow-x-auto bg-[#F9FBFD] px-4 print:p-0 print:bg-white print:overflow-visible ">
+      <div className="min-w-max flex justify-center w-[816px] py-4 print:py-0 mx-auto print:w-full print:min-w-0">
+        <EditorContent editor={editor} />
+      </div>
+    </div>
+  );
+};
+
+export default Editor;
---
+++ src/app/documents/[documentId]/toolbar.tsx
@@ -0,0 +1,160 @@
+"use client";
+import {
+  LucideIcon,
+  Undo2Icon,
+  Redo2Icon,
+  PrinterIcon,
+  SpellCheckIcon,
+  BoldIcon,
+  ItalicIcon,
+  UnderlineIcon,
+  MessageSquarePlusIcon,
+  ListTodoIcon,
+  RemoveFormattingIcon,
+} from "lucide-react";
+import { cn } from "@/lib/utils";
+import { useEditorState } from "@/store/use-editor-store";
+import { Separator } from "@/components/ui/separator";
+import {
+  FontFamilyButton,
+  HeadingLevelButton,
+  TextColorButton,
+  HighlightColorButton,
+  LinkButton,
+  ImageButton,
+  AlignButton,
+  ListButton,
+  FontSizeButton,
+} from "@/app/documents/components/toolbarButton";
+
+interface toolbarButtonProps {
+  onClick?: () => void;
+  isActive?: boolean;
+  icon: LucideIcon;
+}
+
+// 每个按钮组件
+const ToolbarButton = ({
+  onClick,
+  isActive,
+  icon: Icon,
+}: toolbarButtonProps) => {
+  return (
+    <button
+      onClick={onClick}
+      className={cn(
+        "flex text-sm h-7 min-w-7 items-center justify-center rounded-sm hover:bg-neutral-200/80",
+        isActive && "bg-neutral-200/80"
+      )}
+    >
+      <Icon className="size-4" />
+    </button>
+  );
+};
+
+export const Toolbar = () => {
+  const { editor } = useEditorState();
+  // console.log("Toolbar Editor", { editor });
+  const sections: {
+    label: string;
+    icon: LucideIcon;
+    onClick: () => void;
+    isActive?: boolean;
+  }[][] = [
+      [
+        {
+          label: "Undo",
+          icon: Undo2Icon,
+          onClick: () => editor?.chain().focus().undo().run(),
+        },
+        {
+          label: "Redo",
+          icon: Redo2Icon,
+          onClick: () => editor?.chain().focus().redo().run(),
+        },
+        {
+          label: "Print",
+          icon: PrinterIcon,
+          onClick: () => window.print(),
+        },
+        {
+          label: "Spell Check",
+          icon: SpellCheckIcon,
+          onClick: () => {
+            const current = editor?.view.dom.getAttribute("spellcheck");
+            editor?.view.dom.setAttribute(
+              "spellcheck",
+              current === "false" ? "true" : "false"
+            );
+          },
+        },
+      ],
+      [
+        {
+          label: "Bold",
+          icon: BoldIcon,
+          isActive: editor?.isActive("bold"),
+          onClick: () => editor?.chain().focus().toggleBold().run(),
+        },
+        {
+          label: "Italic",
+          icon: ItalicIcon,
+          isActive: editor?.isActive("italic"),
+          onClick: () => editor?.chain().focus().toggleItalic().run(),
+        },
+        {
+          label: "Underline",
+          icon: UnderlineIcon,
+          isActive: editor?.isActive("underline"),
+          onClick: () => editor?.chain().focus().toggleUnderline().run(),
+        },
+      ],
+      [
+        {
+          label: "Comment",
+          icon: MessageSquarePlusIcon,
+          isActive: editor?.isActive("underline"),
+          onClick: () => console.log("comment"),
+        },
+        {
+          label: "List Todo",
+          icon: ListTodoIcon,
+          isActive: editor?.isActive("taskList"),
+          onClick: () => editor?.chain().focus().toggleTaskList().run(),
+        },
+        {
+          label: "Remove Formatting",
+          icon: RemoveFormattingIcon,
+          onClick: () => editor?.chain().focus().unsetAllMarks().run(),
+        },
+      ],
+    ];
+
+  return (
+    <div className="bg-[#F1F4F9] px-2.5 py-0.5 rounded-[24px] min-h-[40px] flex items-center gap-x-0.5 overflow-x-auto">
+      {sections[0].map((item) => {
+        return <ToolbarButton key={item.label} {...item} />;
+      })}
+      <Separator orientation="vertical" className="h-6 bg-neutral-300" />
+      <FontFamilyButton />
+      <Separator orientation="vertical" className="h-6 bg-neutral-300" />
+      <HeadingLevelButton />
+      <FontSizeButton />
+      <Separator orientation="vertical" className="h-6 bg-neutral-300" />
+      {sections[1].map((item) => {
+        return <ToolbarButton key={item.label} {...item} />;
+      })}
+      <TextColorButton />
+      <HighlightColorButton />
+      <Separator orientation="vertical" className="h-6 bg-neutral-300" />
+      <LinkButton />
+      <ImageButton />
+      <AlignButton />
+      <ListButton />
+      <Separator orientation="vertical" className="h-6 bg-neutral-300" />
+      {sections[2].map((item) => {
+        return <ToolbarButton key={item.label} {...item} />;
+      })}
+    </div>
+  );
+};
